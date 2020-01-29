cc50b8a7819f26f69aef15451ccc789d00ce5e55
Jorge <jorgediazok@gmail.com>
2020-01-29 13:12:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,147 @@
+//RASTREAR DATOS
+const mejorPelicula = [
+  'Ford v Ferrari',
+  'The Irishman',
+  'Jojo Rabbit',
+  'Joker',
+  'Little Women',
+  'Marriage Story',
+  1917,
+  'Once Upon a Time in Hollywood',
+  'Parasite'
+];
+
+const mejorDirector = [
+  'Martin Scorsese (The Irishman)',
+
+  'Todd Phillips (Joker)',
+
+  'Sam Mendes (1917)',
+
+  'Quentin Tarantino (Once Upon a Time in Hollywood)',
+
+  'Bong Joon Ho (Parasite)'
+];
+
+const mejorActriz = [
+  'Cynthia Erivo (Harriet)',
+
+  'Scarlett Johansson (Marriage Story)',
+
+  'Saoirse Ronan (Little Women)',
+
+  'Charlize Theron (Bombshell)',
+
+  'Renée Zellweger (Judy)'
+];
+
+const mejorActor = [
+  'Antonio Banderas (Pain and Glory)',
+
+  'Leonardo DiCaprio (Once Upon a Time in Hollywood)',
+
+  'Adam Driver (Marriage Story)',
+
+  'Joaquin Phoenix (Joker)',
+
+  'Jonathan Pryce (The Two Popes)'
+];
+
+const mejorActorReparto = [
+  'Brad Pitt (Once Upon a Time in Hollywood)',
+
+  'Al Pacino (The Irishman)',
+
+  'Joe Pesci (The Irishman)',
+
+  'Tom Hanks (A Beautiful Day in the Neighborhood)',
+
+  'Anthony Hopkins (The Two Popes)'
+];
+
+const mejorActrizReparto = [
+  'Laura Dern (Marriage Story)',
+
+  'Kathy Bates (Richard Jewell)',
+
+  'Scarlett Johansson (Jojo Rabbit)',
+
+  'Margot Robbie (Bombshell)',
+
+  'Florence Pugh (Little Women)'
+];
+
+const mejorFotografia = [
+  '1917 (Roger Deakins)',
+
+  'The Irishman (Rodrigo Prieto)',
+
+  'Joker (Lawrence Sher)',
+
+  'The Lighthouse (Jarin Blaschke)',
+
+  'Once Upon a Time in Hollywood (Robert Richardson)'
+];
+
+const peliculaExtranjera = [
+  'Parasite (Corea del Sur)',
+
+  'Pain and Glory (España)',
+
+  'Corpus Christi (Polonia)',
+
+  'Honeyland (Macedonia)',
+
+  'Les Miserables (Francia)'
+];
+
+const animacion = [
+  'How to Train Your Dragon: The Hidden World',
+
+  'I Lost My Body',
+
+  'Klaus',
+
+  'Missing Link',
+
+  'Toy Story 4'
+];
+
+//VOTACIÓN
+
+const resultadosVotacion = [
+  {
+    Leandro: [
+      mejorPelicula[3],
+      animacion[2],
+      peliculaExtranjera[3],
+      mejorFotografia[1],
+      mejorActor[2],
+      mejorActorReparto[1],
+      mejorActriz[1],
+      mejorActrizReparto[3],
+      mejorDirector[1]
+    ]
+  },
+  {
+    Cristian: [
+      mejorPelicula[3],
+      animacion[2],
+      peliculaExtranjera[3],
+      mejorFotografia[1],
+      mejorActor[2],
+      mejorActorReparto[1],
+      mejorActriz[1],
+      mejorActrizReparto[3],
+      mejorDirector[1]
+    ]
+  }
+];
+
+//ENVIAR DATOS/YO LOS OBTENGO COMO ADMIN
+
+//INVENTAR RESULTADOS QUIEN GANO
+
+//COMPARAR RESULTADO CON PREDICCIÓN
+
+//DEVOLVER RESULTADOS (ARMAR UNA TABLA CON PUNTOS)
