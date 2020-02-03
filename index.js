2d2cae40f415c4d4c3a7a5735fab61818462a0a8
Jorge <jorgediazok@gmail.com>
2020-02-03 20:53:27 -0300

    cambios otra vez

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ myForm.addEventListener('submit', event => {
     document.getElementById('header').innerHTML = '';
     document.getElementById('header').innerHTML = `<div class="col mb-4">
     <img src="imgs/oscarsbanner-2.png" alt="banner" class="banner-2"/>
-  </div><p class="mensaje-final">Buenísimo ${resultados.enviarData},acá están tus elegidos para que los imprimas y chequees cómo te fue el 9 de febrero. Suerte!</p> 
+  </div><p class="mensaje-final">Buenísimo ${resultados.enviarData},acá están tus elegidos para que los imprimas y chequees cómo te fue el 9 de febrero! :)</p> 
     <br/>
     <div class="botoncito">
     <input type="button" value="Imprimir" onClick="window.print()" class="btn btn-dark">
